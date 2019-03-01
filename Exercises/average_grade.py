94b8439c31b7b28a7490a4114d5d46e4db368811
Dotnest <madnauk@gmail.com>
2019-03-01 18:02:21 +0300

    done

--- Exercises/average_grade.py
+++ Exercises/average_grade.py
@@ -3,10 +3,24 @@
 
 def average_grade(lst):    
     """ Return students' names and their average grades.
-    
     >>> average_grade([['Bob', 56, 80, 72, 90], ['Alice', 60, 88, 44, 70]])
     >>> [['Bob', 74.5], ['Alice', 65.5]]
     """
-
     # your code here
-    #test
+    for student in lst:
+        total = 0
+        count = 0
+        for grade in student:
+            if type(grade) != type("a"):
+                total = total + grade
+                count += 1
+        student[1] = total/count
+        for _ in range(len(student)-2):
+            student.pop()
+        print(student)
+        #print(total)
+        #print(count)
+
+
+average_grade([['Bob', 56, 80, 72, 90], ['Alice', 60, 88, 44, 70]])
+#>>> [['Bob', 74.5], ['Alice', 65.5]]
