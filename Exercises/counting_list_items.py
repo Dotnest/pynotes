7444e4a79cc48edeb85564d74d0fb5448806d01c
Dotnest <madnauk@gmail.com>
2019-03-02 18:22:06 +0300

    done

--- Exercises/counting_list_items.py
+++ Exercises/counting_list_items.py
@@ -9,3 +9,18 @@ def count_items(lst):
     """
 
     # your code here
+
+    items = lst
+    counts = {}
+    for item in items:
+        if item not in counts:
+            counts[item] = 1
+        else:
+            counts[item] += 1
+    print(counts)
+
+count_items(['one', 'two', 'two', 'three', 'three', 'three'])
+#{'one': 1, 'three': 3, 'two': 2}
+
+count_items(['one', 'two', 'two', 'three', 'three', 'three', 'two', 'three', 'three', 'three', 'four', 'dsfdsdfgfd', 1])
+#{'one': 1, 'two': 3, 'three': 6, 'four': 1, 'dsfdsdfgfd': 1, 1: 1}
