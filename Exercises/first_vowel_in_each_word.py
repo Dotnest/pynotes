884f6c3a8ba3259178eb17592e9c399c01ef0bf4
Dotnest <madnauk@gmail.com>
2019-03-03 01:09:33 +0300

    done

--- Exercises/first_vowel_in_each_word.py
+++ Exercises/first_vowel_in_each_word.py
@@ -1,4 +1,4 @@
-def find_first_vowel(S: str) -> str:
+def find_first_vowel(s: str) -> str:
     """ Return the first vowel in each word of a string.
     
     >>> find_first_vowel("The sky's the limit")
@@ -6,3 +6,22 @@ def find_first_vowel(S: str) -> str:
     """
 
     # your code here
+    result = []
+    vowels = ('e', 'a', 'o', 'i', 'u')
+    if s[-1] != " ":
+        s += " "
+    while s.find(' ') != -1:
+        for i in range(s.find(' ')):
+            #print(i, s.find(' '), s)
+            if s[i] in vowels:
+                result.append(s[i])
+                s = s[s.find(' ')+1:]
+                break
+            if i == s.find(' ')-1:
+                s = s[s.find(' ')+1:]
+    print(result)
+
+
+
+find_first_vowel("The sky's the limit")
+find_first_vowel('This is a test of a little programm i made :)')
