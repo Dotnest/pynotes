9a2b5991294b7bcbfa64430c943d782dabec39b1
Dotnest <madnauk@gmail.com>
2019-03-02 18:42:01 +0300

    done

--- Exercises/factorials.py
+++ Exercises/factorials.py
@@ -8,3 +8,14 @@ def factorialize(numbers):
     """
 
     # your code here
+    for n in range(len(numbers)):
+        f = 1
+        while numbers[n] !=1:
+            f *= numbers[n]
+            numbers[n] -= 1
+        numbers[n] = f
+    print(numbers)
+
+
+factorialize([1, 2, 3, 4, 5, 10])
+#>>> [1, 2, 6, 24, 120, 3628800]
