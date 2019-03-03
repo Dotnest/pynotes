e8ab78ecadc23b4891aa537080b72348f13e0f0b
Dotnest <madnauk@gmail.com>
2019-03-03 15:02:52 +0300

    done

--- Exercises/longest_name.py
+++ Exercises/longest_name.py
@@ -13,3 +13,20 @@ def get_longest_name(a_list):
     """
 
     # your code here
+    ln = a_list[0]
+    for name in a_list:
+        if len(name) > len(ln):
+            ln = name
+        elif len(name) == len(ln) and a_list.index(name) != 0:
+            ln = ''
+            for _ in range(len(name)):
+                ln += '*'
+    print(ln)
+
+
+get_longest_name(["Candide", "Jessie", "Kath", "Amity", "Raeanne"])
+    #>>> '******'
+get_longest_name(["Jessie", "Kath", "Amity", "Raeanne"])
+    #>>> 'Raeanne'
+get_longest_name(["Jessie", "Kath", "Amity"])
+    #>>> 'Jessie'
