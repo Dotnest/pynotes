2b0ba4244d0b9d56ca9fd98f60e00e258870247f
Dotnest <madnauk@gmail.com>
2019-03-03 22:38:00 +0300

    done

--- Exercises/replace_characters.py
+++ Exercises/replace_characters.py
@@ -9,3 +9,13 @@ def replace(text, old_chars, new_chars):
     """
 
     # your code here
+    pos = []
+    for i in range(len(old_chars)):
+        pos.append(text.find(old_chars[i]))
+    for i in range(len(old_chars)):
+        text = text[:pos[i]] + new_chars[i] + text[pos[i]+1:]
+    print(text)
+
+
+replace('heWlXo theYreZ', 'WXYZ', 'YZWX')
+    #>>> 'heYlZo theWreX'
