4a5d172f66b6a0ca567d38d4f5e7a3185aa017ec
Dotnest <madnauk@gmail.com>
2019-03-04 21:52:18 +0300

    done

--- Exercises/words_of_certain_length.py
+++ Exercises/words_of_certain_length.py
@@ -9,3 +9,23 @@ def find_words_of_length(number, text):
     """
 
     # your code here
+    textlist = text.split(' ')
+    punctuation = ('!', '?', '?!', ',', '.', ':', ';', '(', ' )', '-')
+    for word in textlist:
+        if word.isnumeric():
+            continue
+        while word.isalpha() != True:
+            for thing in punctuation:
+                word = word.replace(thing, '')
+    result = []
+    for word in textlist:
+        if len(word) == number:
+            result.append(word)
+    print(result)
+
+
+find_words_of_length(4, "this is a string with some four-letter words.")
+    #>>> ['this', 'with', 'some']
+
+
+#'four' in 'four-letter' is not considered a 4 letter word...?
